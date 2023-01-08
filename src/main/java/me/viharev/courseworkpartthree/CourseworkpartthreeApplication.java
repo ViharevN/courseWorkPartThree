bdd9297c92c9f01c35e2809bc1f3966cf8546890
ViharevN <swix_off@mail.ru>
2023-01-08 21:33:55 +0300

    start

---
+++ src/main/java/me/viharev/courseworkpartthree/CourseworkpartthreeApplication.java
@@ -0,0 +1,13 @@
+package me.viharev.courseworkpartthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkpartthreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseworkpartthreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/viharev/courseworkpartthree/CourseworkpartthreeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.viharev.courseworkpartthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkpartthreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
